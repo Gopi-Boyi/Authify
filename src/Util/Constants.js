d8701c0becf1cede3d07631fb9d15669f813fd94
Gopi Boyi <gopiboyi88@gmail.com>
2026-06-07 15:44:10 +0530

    update API URL to Railway

--- src/Util/Constants.js
+++ src/Util/Constants.js
@@ -1,3 +1,3 @@
-export const AppConstants= {
-    BACKEND_URL: 'http://localhost:8080/api/v1.0'
+export const AppConstants = {
+    BACKEND_URL: 'https://authify-backend-production-50ef.up.railway.app/api/v1.0'
 }
